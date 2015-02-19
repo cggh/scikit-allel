0d74a340cc1361250e8b3c20bcef4bcb17d3e771
Alistair Miles <alimanfoo@googlemail.com>
2015-02-19 18:26:50 +0000

    ready for release

--- allel/stats.py
+++ allel/stats.py
@@ -854,7 +854,8 @@ def heterozygosity_expected(af, ploidy, fill=np.nan):
 
     # fill values where allele frequencies could not be calculated
     af_sum = np.sum(af, axis=1)
-    out[(af_sum < 1) | np.isnan(af_sum)] = fill
+    with ignore_invalid():
+        out[(af_sum < 1) | np.isnan(af_sum)] = fill
 
     return out
 
