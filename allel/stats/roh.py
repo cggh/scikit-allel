b772a931554e1714c14cd1ff7c14abbe1545eaf0
Nick Harding <njh@well.ox.ac.uk>
2016-09-07 17:10:49 +0100

    Fixes to homerun
    > modified docs
    > split into phet_roh and non roh
    > removed chunking
    > fixed paccess error
    > added plot

--- allel/stats/roh.py
+++ allel/stats/roh.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, division
-
 import numpy as np
-from hmmlearn import hmm
-from allel.chunked import ChunkedArray
 
 
-def call_roh(g, pos, ix, emission=(0.001, 0.0025, 0.01),
+def call_roh(g, pos, ix, phet_roh=0.001, phet_nonroh=(0.0025, 0.01),
              transition=1e-6, min_roh=100000,
-             is_accessible=None, contig_size=None):
+             is_accessible=None, contig_size=None, plot=True,
+             plotwinsize=50000):
 
     """
-    Call ROH (runs of homozygosity) of a given individual
+    Call ROH (runs of homozygosity) of a individual given a genotype array
+    This function computes the likely ROH using an HMM model. There are 3
+    observable states: 0 = Hom, 1 = Het, 2 = inaccessible (ie unobserved).
+
+    The model is provided with a probability of observing a het in a ROH (
+    phet_roh) and one or more probabilities of observing a het in a non-ROH,
+    as this probability may not be constant across the genome (phet_nonroh).
 
     Parameters
     ----------
@@ -21,11 +25,15 @@ def call_roh(g, pos, ix, emission=(0.001, 0.0025, 0.01),
         Positions of variants, same 0th dimension as g
     ix: integer
         index of the sample on which to call roh
-    emission: tuple or array_like, float
-        Probability of observing a heterozygote in each hidden state.
-        Supports >= 2 states. Appropriate values will depend on
-        frequency of sequencing error & spurious het calls due
-        to misalignment
+    phet_roh: float
+        Probability of observing a heterozygote in an ROH. Appropriate values
+        will depend on de novo mutation rate, frequency of sequencing error &
+        spurious het calls due to misalignment.
+    phet_nonroh: tuple, array_like, float
+        One or more probabilites of observing a heterozygote in an non ROH.
+        Appropriate values will depend on population history, frequency of
+        sequencing error, mutation rate, and spurious het calls due to
+        misalignment.
     transition: float
         Probability of moving between states
     min_roh: integer
@@ -37,6 +45,11 @@ def call_roh(g, pos, ix, emission=(0.001, 0.0025, 0.01),
     contig_size: int
         If is_accessible not known/not provided, allows specification of
         total length of contig.
+    plot: bool
+        Whether to plot the ROH with heterzygosity and inaccessible regions
+    plotwinsize: int
+        Size of window in which to summarize heterozygosity for the plot
+
 
     Returns
     -------
@@ -47,15 +60,12 @@ def call_roh(g, pos, ix, emission=(0.001, 0.0025, 0.01),
 
     """
 
+    from hmmlearn import hmm
 
-    # this function computes the likely ROH using an HMM model.
-    # define model:
-    ### 2 state model, 3 observable
-    # 0: ROH
-    # 1: normal
-    # 2: inaccessible
-    emission_px = np.array(emission)
-    emission_px.sort()
+    if isinstance(phet_nonroh, float):
+        phet_nonroh = np.array((phet_nonroh,))
+
+    emission_px = np.concatenate([(phet_roh, ), phet_nonroh])
 
     # start probability
     start_prob = np.repeat(1/emission_px.size, emission_px.size)
@@ -67,10 +77,13 @@ def call_roh(g, pos, ix, emission=(0.001, 0.0025, 0.01),
     if is_accessible is None:
         assert contig_size is not None, \
             "If accessibility not provided must specify size of contig"
-        # may be a mem problem?
-        is_accessible = ChunkedArray(np.repeat(True, contig_size))
+        p_accessible = 1.0
+    else:
+        p_accessible = is_accessible.mean()
+        assert contig_size is None, "Contig size only specified when " \
+                                    "is_accessible is not provided"
+        contig_size = is_accessible.size
 
-    p_accessible = is_accessible.mean()
     emission_mx = _derive_emission_mx(emission_px, p_accessible)
 
     # initialize HMM
@@ -81,34 +94,92 @@ def call_roh(g, pos, ix, emission=(0.001, 0.0025, 0.01),
     roh_hmm.transmat_ = transitions
     roh_hmm.emissionprob_ = emission_mx
 
-    is_heterozygote = g.is_het()
+    gsub = g.take([ix], axis=1)
+    hz = gsub.count_het(axis=1).astype("bool")
 
-    hz = is_heterozygote[:, ix]
-    pred, obs = _predict_roh_state(roh_hmm, hz, pos, is_accessible)
+    pred, obs = _predict_roh_state(roh_hmm, hz, pos, is_accessible, contig_size)
     homozygous_windows = _get_state_windows(pred, state=0)
 
     # filter by roh size
     roh_sizes = np.diff(homozygous_windows, axis=1).flatten()
     roh = np.compress(roh_sizes >= min_roh, homozygous_windows, axis=0)
+    froh = np.diff(roh, axis=1).sum() / contig_size
+
+    if plot:
+        import matplotlib.pyplot as plt
+        from allel.stats import windowed_statistic
+
+        window = np.arange(0, is_accessible.size, plotwinsize)
+        windows = np.vstack([window[:-1], window[1:]]).T
+
+        count_hz, win, count = windowed_statistic(pos=pos, values=hz,
+                                                  statistic=np.sum,
+                                                  windows=windows)
+
+        if is_accessible is not None:
+
+            # remove the last val as not a complete window
+            count_acc = np.add.reduceat(
+                is_accessible, window)[:-1]
+
+            # if accessible bases in a window < 1000, don't plot.
+            # b/c Leads to spiky data
+            count_acc[count_acc < 1000] = 0
+            count_hz[count_acc < 1000] = 0
+            heterozyg = count_hz/count_acc
+
+            assert not np.any(count_hz > count_acc), \
+                "heterozygosity exceeds 1.0. Check accessibility array input"
+        else:
+            heterozyg = count_hz/plotwinsize
+
+        x = win.mean(axis=1) * 1e-6
+
+        plt.figure(figsize=(6, 1.5))
+        plt.plot(x, heterozyg, "b-", label="Heterozygosity")
+        plt.ylabel('Heterozygosity')
+
+        for low, upp in roh * 1e-6:
+            plt.axvspan(low, upp, facecolor='g', alpha=0.5, edgecolor='none')
+
+        if is_accessible is not None:
+            inaccessible_windows = _get_state_windows(is_accessible)
+
+            for low, upp in (inaccessible_windows * 1e-6):
+                # don't plot inaccessible gaps < 5kb
+                if (upp - low) > .005:
+                    plt.axvspan(low, upp, facecolor='m', alpha=0.1,
+                                edgecolor='none')
+
+        plt.xlabel('Position (Mbp)')
+        plt.title(" ".join(['Called runs of homozygosity.',
+                            r'$F_{ROH}:$',
+                            "{0:.4f}".format(froh)]))
+        plt.tick_params(top="off", right="off")
+        plt.legend()
 
     # return roh and froh
-    return roh, np.diff(roh, axis=1).sum() / is_accessible.size
+    return roh, froh
 
 
-def _predict_roh_state(model, is_het, pos, accessible):
+def _predict_roh_state(model, is_het, pos, is_accessible=None,
+                       contig_size=None):
 
     # assume non-included pos are homozygous reference
     assert is_het.shape == pos.shape
 
     # declaration of this object may result in large mem usage
-    observations = ChunkedArray(
-        np.zeros((accessible.size, 1), dtype='int'))
+    if is_accessible is not None:
+        contig_size = is_accessible.size
 
-    for i in np.compress(is_het, pos):
-        observations[i - 1] = 1
+    observations = np.zeros((contig_size, 1), dtype='int')
 
-    for i in np.where(accessible == 0)[0]:
-        observations[i - 1] = 2
+    # these are hets
+    observations[np.compress(is_het, pos) - 1] = 1
+
+    # these are unobserved
+    if is_accessible is not None:
+        observations[~is_accessible] = 2
 
     predictions = model.predict(X=observations)
     return predictions, observations
@@ -117,7 +188,7 @@ def _predict_roh_state(model, is_het, pos, accessible):
 def _derive_emission_mx(prob, pa):
     # one row per p in prob
     # hom, het, unobserved
-    mx = [[(1-p) * (1-pa), p * (1-pa), pa] for p in prob]
+    mx = [[(1-p)*pa, p*pa, 1-pa] for p in prob]
     mxe = np.array(mx)
     assert mxe.shape == (prob.size, 3)
     return mxe
@@ -139,26 +210,22 @@ def _derive_transition_mx(pr, nstates):
 # This function translates the yes/no into a set of windows
 def _get_state_windows(predicted_state, state=0):
 
-    assert isinstance(predicted_state, np.ndarray), \
-        "_get_state_windows expects an ndarray"
     wh = np.where(predicted_state == state)[0]
     if wh.size == 0:
         # then there are no things of interest
-        return np.empty(0)
-    elif wh.size == 1:
+        return np.empty((0, 2))
+    elif wh.size == predicted_state.size:
         # the whole thing is one big thing of interest
         return np.array([1, predicted_state.size])
+    else:
+        intervals = list()
+        iv_start = wh[0]
 
-    intervals = list()
-    iv_start = wh[0]
-
-    pos = None
-    for i, pos in enumerate(wh[1:]):
-        if (pos - wh[i]) > 1:
-            intervals.append([iv_start, wh[i]])
-            iv_start = pos
-    intervals.append([iv_start, pos])
-
-    roh = np.array(intervals)
-    # correct for fact that pos 1 is in index 0.
-    return roh + 1
+        for i, pos in enumerate(wh[1:]):
+            if (pos - wh[i]) > 1:
+                intervals.append([iv_start, wh[i]])
+                iv_start = pos
+        intervals.append([iv_start, pos])
+        roh = np.array(intervals)
+        # correct for fact that pos 1 is in index 0.
+        return roh + 1
