6116d3bc35185862dadf035d39409776beb6e3c3
Nick Harding <njh@well.ox.ac.uk>
2018-03-01 14:48:41 +0000

    Finalize, mainly coding standards / PEP8

--- allel/stats/roh.py
+++ allel/stats/roh.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, division
 
-
 import numpy as np
-import pandas as pd
 
 from allel.model.ndarray import GenotypeVector
 from allel.util import asarray_ndim, check_dim0_aligned
@@ -88,7 +86,8 @@ def roh_mhmm(gv, pos, phet_roh=0.001, phet_nonroh=(0.0025, 0.01), transition=1e-
     # probability of inaccessible
     if is_accessible is None:
         if contig_size is None:
-            raise ValueError("If is_accessibile argument is not provided, you must provide contig_size")
+            raise ValueError(
+                "If is_accessibile argument is not provided, you must provide contig_size")
         p_accessible = 1.0
     else:
         p_accessible = is_accessible.mean()
@@ -152,12 +151,12 @@ def _mhmm_predict_roh_state(model, is_het, pos, is_accessible, contig_size):
 def roh_poissonhmm(gv, pos, phet_roh=0.001, phet_nonroh=(0.0025, 0.01), transition=1e-3,
                    window_size=1000, min_roh=0, is_accessible=None, contig_size=None):
 
-
     """Call ROH (runs of homozygosity) in a single individual given a genotype vector.
 
-    This function computes the likely ROH using a Poisson HMM model. The chromosome is divided into equally accessible
-    windows of specified size, then the number of hets observed in each is used to fit a Poisson HMM.
-    Note this is much faster than `roh_mhmm`, but at the cost of some resolution.
+    This function computes the likely ROH using a Poisson HMM model. The chromosome is divided into
+    equally accessible windows of specified size, then the number of hets observed in each is used
+    to fit a Poisson HMM. Note this is much faster than `roh_mhmm`, but at the cost of some
+    resolution.
 
     The model is provided with a probability of observing a het in a ROH (`phet_roh`) and one
     or more probabilities of observing a het in a non-ROH, as this probability may not be
@@ -177,21 +176,21 @@ def roh_poissonhmm(gv, pos, phet_roh=0.001, phet_nonroh=(0.0025, 0.01), transiti
         Appropriate values will depend primarily on nucleotide diversity within
         the population, but also on mutation rate and genotype error rate.
     transition: float, optional
-        Probability of moving between states. This is based on windows, so a larger window size may call for a
-        larger transitional probability
+        Probability of moving between states. This is based on windows, so a larger window size may
+        call for a larger transitional probability
     window_size: integer, optional
-        Window size (equally accessible bases) to consider as a potential ROH.
-        and recombination rate. Setting this window too small may result in spurious ROH calls, while too large will
-        result in a lack of resolution.
+        Window size (equally accessible bases) to consider as a potential ROH. Setting this window
+        too small may result in spurious ROH calls, while too large will result in a lack of
+        resolution.
     min_roh: integer, optional
         Minimum size (bp) to condsider as a ROH. Will depend on contig size and recombination rate.
     is_accessible: array_like, bool, shape (`contig_size`,), optional
         Boolean array for each position in contig describing whether accessible
-        or not. Although optional, highly recommended so invariant sites are distinguishable from sites where variation
-        is inaccessible
+        or not. Although optional, highly recommended so invariant sites are distinguishable from
+        sites where variation is inaccessible
     contig_size: integer, optional
-        If is_accessible is not available, use this to specify the size of the contig, and assume all sites are
-        accessible.
+        If is_accessible is not available, use this to specify the size of the contig, and assume
+        all sites are accessible.
 
 
     Returns
@@ -204,8 +203,7 @@ def roh_poissonhmm(gv, pos, phet_roh=0.001, phet_nonroh=(0.0025, 0.01), transiti
 
     Notes
     -----
-    This function requires `pomegranate` (>= 0.9.0) and `intervaltree` to be installed.
-    installed.
+    This function requires `pomegranate` (>= 0.9.0) to be installed.
 
     """
 
@@ -214,7 +212,8 @@ def roh_poissonhmm(gv, pos, phet_roh=0.001, phet_nonroh=(0.0025, 0.01), transiti
     # equally accessbile windows
     if is_accessible is None:
         if contig_size is None:
-            raise ValueError("If is_accessibile argument is not provided, you must provide contig_size")
+            raise ValueError(
+                "If is_accessibile argument is not provided, you must provide contig_size")
         is_accessible = np.ones((contig_size,), dtype="bool")
     else:
         contig_size = is_accessible.size
@@ -251,7 +250,7 @@ def roh_poissonhmm(gv, pos, phet_roh=0.001, phet_nonroh=(0.0025, 0.01), transiti
 
     # filter by ROH size
     if min_roh > 0:
-       df_roh = df_roh[df_roh.length >= min_roh]
+        df_roh = df_roh[df_roh.length >= min_roh]
 
     # compute FROH
     froh = df_roh.length.sum() / contig_size
--- docs/stats/roh.rst
+++ docs/stats/roh.rst
@@ -3,3 +3,4 @@ Runs of homozygosity (ROH)
 
 .. automodule:: allel.stats.roh
 .. autofunction:: allel.roh_mhmm
+.. autofunction:: allel.roh_poissonhmm
