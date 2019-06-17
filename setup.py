d16db1c0fdfa5e04d24ef824459e5fc938ffd6f9
Alistair Miles <alimanfoo@googlemail.com>
2019-06-17 10:02:08 +0100

    drop py35 testing (not supported on conda-forge)

--- setup.py
+++ setup.py
@@ -46,7 +46,6 @@ CLASSIFIERS = [
     'Operating System :: Unix',
     'Operating System :: MacOS',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
 ]
